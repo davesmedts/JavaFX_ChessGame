e611abf29edab6c4389dc25f5a06e0007a5b968a
dsmedts <dave.smedts@gmail.com>
2022-02-11 14:31:39 +0100

    constructor game, verplaatsen List<moves> naar piece

--- Chess/src/Model/Game.java
+++ Chess/src/Model/Game.java
@@ -1,18 +1,38 @@
 package Model;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
-/**
- * @author Dave Smedts
- * @version 1.0 2/02/2022 16:58
- */
 public class Game {
     private Player blackPlayer;
     private Player whitePlayer;
     private Board gameBoard;
     private Color turn;
-    private LocalDateTime startTijd;
-    private LocalDateTime stopTijd;
+    private LocalDateTime startTime;
+    private LocalDateTime stopTime;
     private Player winner;
 
+    //constructor
+    public Game(Player playerOne, Player playerTwo) {
+        Random rd = new Random();
+        boolean playerOneIsBlack = rd.nextBoolean(); // creating Random boolean
+        if(playerOneIsBlack){
+            blackPlayer = playerOne;
+            whitePlayer = playerTwo;
+        } else {
+            blackPlayer = playerTwo;
+            whitePlayer = playerOne;
+        }
+        gameBoard = new Board();
+        turn = Color.WHITE;
+        startTime = LocalDateTime.now();
+    }
+
+    public void setStopTime(LocalDateTime stopTime) {
+        this.stopTime = stopTime;
+    }
+
+    public void setWinner(Player winner) {
+        this.winner = winner;
+    }
 }
--- Chess/src/Model/Piece.java
+++ Chess/src/Model/Piece.java
@@ -1,5 +1,8 @@
 package Model;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Dave Smedts
  * @version 1.0 1/02/2022 23:15
@@ -8,7 +11,22 @@ public class Piece {
     private Color color;
     private Square startPosition;
     private Square position;
-    private int moveCount;
+//    private int moveCount; Dit attribuut kan weg volgens mij, als we de lengte van de "List<Square> moves;" gebruiken om te bepalen of bv een koning al verplaatst is of niet.
+    private List<Square> moves;
     private boolean isCaptured;
+
+
+    public Square[] getValidMoves(){
+
+    }
+
+    public boolean Move(Square targetSquare){
+        List<Square> validMoves = Arrays.asList(getValidMoves());
+        if(validMoves.contains(targetSquare)){
+            position = targetSquare;
+            moves.add(targetSquare);
+            return true;
+        } return false;
+    }
 
 }
