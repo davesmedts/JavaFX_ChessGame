b07abad4d969c7118578c966c5bb8fc8d27760bb
Dave.smedts <73465667+davesmedts@users.noreply.github.com>
2022-02-13 23:27:55 +0100

    toString square

--- Chess/src/Main.java
+++ Chess/src/Main.java
@@ -6,17 +6,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-
         System.out.println("Deef is Deef");
     }
-
-    private void fillColumnsAndRows() {
-        for (int i = 0; i < columns.length; i++) {
-            char rowLetter = (char)(65 + i); //ASCII code gebruiken om int naar char om te zetten
-            columns[i] = rowLetter;
-            rows[i] = i +1;
-        }
-    }
-
 
 }
