13e07e69269f212cc9ef2b249a5ce49d5aaeca1a
Dave.smedts <73465667+davesmedts@users.noreply.github.com>
2022-02-14 00:43:49 +0100

    initializePieces (pawn)

--- Chess/src/Model/Player.java
+++ Chess/src/Model/Player.java
@@ -8,20 +8,43 @@ public class Player {
     private int draws;
     private int losses;
     private Color color;
-    private List<Piece> activePieces;
-    private List<Piece> capturedPieces;
+    private List<Piece> pieces;
+//    private List<Piece> capturedPieces;
 
-    public void initializePieces(){
+    public void initializePieces() {
 //        pawns
-//        for (int i = 0; i < 8; i++) {
-//
-//        }
+        if (color == Color.WHITE) {
+
+            for (int i = 0; i < 8; i++) {
+                int row = 2;
+                char column = (char) (65 + i);
+                Square position = new Square(row, column);
+                Pawn pawn = new Pawn(color, position);
+                pieces.add(pawn);
+
+            }
 //        king
 //        queen
 //        Knights
 //        rooks
 //        bishops
-    }
+        } else {
+//        pawns
+            for (int i = 0; i < 8; i++) {
+                int row = 7;
+                char column = (char) (65 + i);
+                Square position = new Square(row, column);
+                Pawn pawn = new Pawn(color, position);
+                pieces.add(pawn);
+
+            }
+//        king
+//        queen
+//        Knights
+//        rooks
+//        bishops
+        }
+}
 
     public void setColor(Color color) {
         this.color = color;
