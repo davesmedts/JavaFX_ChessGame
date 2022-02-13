7a0654fd88f130d88daad9c97600e585d26283f6
Dave.smedts <73465667+davesmedts@users.noreply.github.com>
2022-02-13 22:19:02 +0100

    git acties testen.

--- Chess/src/Main.java
+++ Chess/src/Main.java
@@ -6,8 +6,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-
         System.out.println("Deef is Deef");
+        System.out.println("Tekstje voor mattia");
     }
-
 }
