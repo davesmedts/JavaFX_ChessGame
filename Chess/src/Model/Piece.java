c4d4d5192b3ad55171a3635c879cf5604a9e2231
Mattia Verreydt <mattiaver@hotmail.com>
2022-02-15 21:24:01 +0100

    stap valid move in orde gebracht, er wordt nu gecontroleerd dat er geen eigen piece op de eindpositie staat

--- Chess/src/Model/Piece.java
+++ Chess/src/Model/Piece.java
@@ -1,5 +1,6 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,32 +11,63 @@ public class Piece {
     private List<Square> moves;
     private boolean isCaptured;
 
+
+    private List<Piece> blackPieces = new ArrayList<>(); // volgens mij hebben we dit nodig voor onze get
+    private List<Piece> whitePieces = new ArrayList<>(); // volgens mij hebben we dit nodig voor onze get
+
     public Piece(Color color, Square startPosition) {
         this.color = color;
         this.startPosition = startPosition;
     }
 
+
+    public void addList(Piece piece) {
+        if (piece.color == Color.valueOf("white")) {
+            whitePieces.add(piece);
+        } else {
+            blackPieces.add(piece);
+        }
+    } // Hoe kunnen we die pieces het best toevoegen aan dieje list? Want zonder dieje list kunnen wij die pieces toch niet getten?
+
     public Color getColor() {
         return color;
     } // getting the color attribute of a Piece, need it for the comparison black-white piece
 
-    public Square[] getValidMoves(){
+    public Square[] getValidMoves() {
         return new Square[0];
     }
 
-    public boolean Move(Square targetSquare){
+    public boolean Move(Square targetSquare) {
         List<Square> validMoves = Arrays.asList(getValidMoves());
-        if(validMoves.contains(targetSquare)){
+        if (validMoves.contains(targetSquare)) {
             endPosition = targetSquare;
             moves.add(targetSquare);
             return true;
-        } return false;
+        }
+        return false;
     }
 
-    public boolean getPiece(Square endPosition, Color color){
 
-        if(endPosition ==  )
-        return ;
+    public Boolean getPiece(Square endPosition, Color color) {
+
+        if (color == Color.valueOf("white")) {
+            for (int i = 0; i < whitePieces.size(); i++) {
+                Piece piece = whitePieces.get(i);
+                if (piece.startPosition == endPosition) {
+                    return true;
+
+                } //we will loop and compare first the color of the piece and then the startposition of the piece.
+            }
+        } else {
+            for (int i = 0; i < blackPieces.size(); i++) {
+                Piece piece = blackPieces.get(i);
+                if (piece.startPosition == endPosition) {
+                    return true;
+
+                }
+            }
+
+        }
+        return false;
     }
-
 }
--- Chess/src/Model/Rook.java
+++ Chess/src/Model/Rook.java
@@ -11,18 +11,21 @@ public class Rook extends Piece {
     } //Constructor to create a rook piece, constructor calls the constructor of the superclass Piece
 
 
-    public Square[] getValidMoves(Square startPosition, Square endPosition) {
+    public Square[] getValidMoves(Square startPosition, Square endPosition, Piece piece) {
 
 
         // er mag geen eigen piece op de plek staan waar we heen willen
+        Boolean possiblePiece = getPiece(endPosition, piece.getColor());
+        if (possiblePiece){
+            System.out.println("No valid move, already a piece of your color is on that space"); // kunnen dit mss cleaner oplossen met een exception?
+        }
 
-        boolean possiblePiece = getPiece(endPosition, this.getColor());
+            //code schrijven om de rook horizontaal en verticaal te laten bewegn
 
-        //code schrijven om de rook horizontaal en verticaal te laten bewegn
 
 //code schrijven om rook niet over eigen pieces te laten springen
 
-        return new Square[0];
+            return new Square[0];
 
     } //We calculate every valid move for the rook, by doing a constructor overload
 }
