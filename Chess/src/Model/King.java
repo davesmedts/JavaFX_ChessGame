7dfb4a578ee33f85ffe40170b2b26e0bd93ef88f
Dave.smedts <73465667+davesmedts@users.noreply.github.com>
2022-02-20 11:39:25 +0100

    getValidMoves() King + verbeteringen GetValidMoves() Knight

--- Chess/src/Model/King.java
+++ Chess/src/Model/King.java
@@ -1,10 +1,13 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Dave Smedts
  * @version 1.0 9/02/2022 22:37
  */
-public class King extends Piece{
+public class King extends Piece {
     private boolean isChecked;
     private boolean isCheckmate;
     private boolean HasCastled;
@@ -14,5 +17,64 @@ public class King extends Piece{
         this.isChecked = false;
         this.isCheckmate = false;
         HasCastled = false;
+    }
+
+    @Override
+    public List<Square> getValidMoves() {
+        List<Square> validSquares = new ArrayList<>();
+//        get the current row and column of the piece that has to move
+        char column = super.getPosition().getColumnLetter();
+        int row = super.getPosition().getRowNumber();
+
+//        a King can move in any direction but is limited to the square next to it.
+//        move up
+        if (row + 1 <= 8) { // check if target is whithin the scope of the board.
+            int newRow = row + 1;
+            char newColumn = column;
+            validSquares.add(new Square(newRow, newColumn));
+        }
+//        move down
+        if (row - 1 >= 1) { // check if target is whithin the scope of the board.
+            int newRow = row - 1;
+            char newColumn = column;
+            validSquares.add(new Square(newRow, newColumn));
+        }
+//        move left
+        if (column - 1 >= 65) { // check if target is whithin the scope of the board.
+            int newRow = row;
+            char newColumn = (char) (column - 1);
+            validSquares.add(new Square(newRow, newColumn));
+        }
+//        move right
+        if (column + 1 < 65 + 8) { // check if target is whithin the scope of the board.
+            int newRow = row;
+            char newColumn = (char) (column + 1);
+            validSquares.add(new Square(newRow, newColumn));
+        }
+//        move right-up
+        if (row + 1 <= 8 || column + 1 < 65 + 8) { // check if target is whithin the scope of the board.
+            int newRow = row + 1;
+            char newColumn = (char) (column + 1);
+            validSquares.add(new Square(newRow, newColumn));
+        }
+//        move right-down
+        if (row - 1 >= 1 || column + 1 < 65 + 8) { // check if target is whithin the scope of the board.
+            int newRow = row - 1;
+            char newColumn = (char) (column + 1);
+            validSquares.add(new Square(newRow, newColumn));
+        }
+//        move left-up
+        if (row + 1 <= 8 || column - 1 >= 65) { // check if target is whithin the scope of the board.
+            int newRow = row + 1;
+            char newColumn = (char) (column - 1);
+            validSquares.add(new Square(newRow, newColumn));
+        }
+//        move left-down
+        if (row - 1 <= 1 || column - 1 >= 65) { // check if target is whithin the scope of the board.
+            int newRow = row - 1;
+            char newColumn = (char) (column - 1);
+            validSquares.add(new Square(newRow, newColumn));
+        }
+        return validSquares;
     }
 }
--- Chess/src/Model/Knight.java
+++ Chess/src/Model/Knight.java
@@ -10,11 +10,12 @@ public class Knight extends Piece {
 
     public List<Square> getValidMoves() {
         List<Square> validSquares = new ArrayList<>();
+//        get the current row and column of the piece that has to move
         char column = super.getPosition().getColumnLetter();
         int row = super.getPosition().getRowNumber();
 //        A knight always first moves 2 squares and the takes a turn of one square. You need to read the comments below as"move <2squares> <1square>"
 //        move up left
-        if (row + 2 <= 8 || column - 1 < 65 + 8) { // check if target is whithin the scope of the board.
+        if (row + 2 <= 8 || column - 1 >= 65) { // check if target is whithin the scope of the board.
             int newRow = row + 2;
             char newColumn = (char) ((char) column - 1);
             validSquares.add(new Square(newRow, newColumn));
@@ -26,25 +27,25 @@ public class Knight extends Piece {
             validSquares.add(new Square(newRow, newColumn));
         }
 //         move down left
-        if (row - 2 <= 8 || column - 1 < 65 + 8) {
+        if (row - 2 >= 1 || column - 1 >= 65) {
             int newRow = row - 2;
             char newColumn = (char) ((char) column - 1);
             validSquares.add(new Square(newRow, newColumn));
         }
 //         move down right
-        if (row - 2 <= 8 || column + 1 < 65 + 8) {
+        if (row - 2 >= 1 || column + 1 < 65 + 8) {
             int newRow = row - 2;
             char newColumn = (char) ((char) column + 1);
             validSquares.add(new Square(newRow, newColumn));
         }
 //        move left up
-        if (row + 1 <= 8 || column - 2 < 65 + 8) {
+        if (row + 1 <= 8 || column - 2 >= 65) {
             int newRow = row + 1;
             char newColumn = (char) ((char) column - 2);
             validSquares.add(new Square(newRow, newColumn));
         }
 //        move left down
-        if (row - 1 <= 8 || column - 2 < 65 + 8) {
+        if (row - 1 >= 1 || column - 2 >= 65) {
             int newRow = row - 1;
             char newColumn = (char) ((char) column - 2);
             validSquares.add(new Square(newRow, newColumn));
@@ -56,7 +57,7 @@ public class Knight extends Piece {
             validSquares.add(new Square(newRow, newColumn));
         }
 //        move right down
-        if (row - 1 <= 8 || column + 2 < 65 + 8) {
+        if (row - 1 >= 1 || column + 2 < 65 + 8) {
             int newRow = row - 1;
             char newColumn = (char) ((char) column + 2);
             validSquares.add(new Square(newRow, newColumn));
